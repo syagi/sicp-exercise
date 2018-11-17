4f4e91356044f9fe2a1bbc2e3bda0ac747827c2a
Shinnosuke Yagi <linus404@gmail.com>
2018-11-17 20:58:55 +0900

    SICP 5.2

---
+++ 5.11c.rkt
@@ -0,0 +1,413 @@
+#lang racket
+(require r5rs)
+
+(define (make-machine register-names ops controller-text)
+  (let ((machine (make-new-machine)))
+    (for-each (lambda (register-name)
+                ((machine 'allocate-register) register-name))
+              register-names)
+    ((machine 'install-operations) ops)    
+    ((machine 'install-instruction-sequence)
+     (assemble controller-text machine))
+    machine))
+
+;;レジスタ
+(define (make-register name)
+  (let ((contents '*unassigned*)
+        (stack (make-stack)))
+    (define (dispatch message)
+      (cond ((eq? message 'get) contents)
+            ((eq? message 'set)
+             (lambda (value) (set! contents value)))
+            ((eq? message 'push) (stack 'push))
+            ((eq? message 'pop) (stack 'pop))
+            ((eq? message 'initialize) (stack 'initialize))
+            (else
+             (error "Unknown request -- REGISTER" message))))
+    dispatch))
+
+(define (get-contents register)
+  (register 'get))
+
+(define (set-contents! register value)
+  ((register 'set) value))
+
+;;スタック
+(define (make-stack)
+  (let ((s '()))
+    (define (push x)
+      (set! s (cons x s)))
+    (define (pop)
+      (if (null? s)
+          (error "Empty stack -- POP")
+          (let ((top (car s)))
+            (set! s (cdr s))
+            top)))
+    (define (initialize)
+      (set! s '())
+      'done)
+    (define (dispatch message)
+      (cond ((eq? message 'push) push)
+            ((eq? message 'pop) (pop))
+            ((eq? message 'initialize) (initialize))
+            (else (error "Unknown request -- STACK"
+                         message))))
+    dispatch))
+
+(define (pop stack)
+  (stack 'pop))
+
+
+(define (push stack value)
+  ((stack 'push) value))
+
+;; 計算機
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '()))
+    (let ((the-ops
+           (list (list 'initialize-stack
+                       (lambda () (stack 'initialize)))))
+          (register-table
+           (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register: " name)
+              (let ((reg (make-register name)))
+                (set! register-table
+                  (cons (list name reg)
+                        register-table))
+                (reg 'initialize))) ;initialzie the stack of allocated register
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (error "Unknown register:" name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+                ((instruction-execution-proc (car insts)))
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq) (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register) allocate-register)
+              ((eq? message 'get-register) lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops) (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              (else (error "Unknown request -- MACHINE" message))))
+      dispatch)))
+
+;; test用
+(define (start machine)
+  (machine 'start))
+
+(define (get-register-contents machine register-name)
+  (get-contents (get-register machine register-name)))
+
+(define (set-register-contents! machine register-name value)
+  (set-contents! (get-register machine register-name) value)
+  'done)
+
+(define (get-register machine reg-name)
+  ((machine 'get-register) reg-name))
+
+
+;; アセンブラ
+(define (assemble controller-text machine)
+  (extract-labels controller-text
+    (lambda (insts labels)
+      (update-insts! insts labels machine)
+      insts)))
+
+(define (extract-labels text receive)
+  (if (null? text)
+      (receive '() '())
+      (extract-labels (cdr text)
+       (lambda (insts labels)
+         (let ((next-inst (car text)))
+           (if (symbol? next-inst)
+             (if (assoc next-inst labels)
+                 (error "label already exists :" next-inst)
+                 (receive insts
+                          (cons (make-label-entry next-inst
+                                                  insts)
+                                labels)))
+                 (receive (cons (make-instruction next-inst)
+                                insts)
+                          labels)))))))
+
+(define (update-insts! insts labels machine)
+  (let ((pc (get-register machine 'pc))
+        (flag (get-register machine 'flag))
+        (stack (machine 'stack))
+        (ops (machine 'operations)))
+    (for-each
+     (lambda (inst)
+       (set-instruction-execution-proc! 
+        inst
+        (make-execution-procedure
+         (instruction-text inst) labels machine
+         pc flag stack ops)))
+     insts)))
+
+(define (make-instruction text)
+  (cons text '()))
+
+
+(define (instruction-text inst)
+  (car inst))
+
+
+(define (instruction-execution-proc inst)
+  (cdr inst))
+
+
+(define (set-instruction-execution-proc! inst proc)
+  (set-cdr! inst proc))
+
+;label
+(define (make-label-entry label-name insts)
+  (cons label-name insts))
+
+(define (lookup-label labels label-name)
+  (let ((val (assoc label-name labels)))
+    (if val
+        (cdr val)
+        (error "Undefined label -- ASSEMBLE" label-name))))
+
+ 
+;; 実行手続き
+  (define (make-execution-procedure inst labels machine
+                                  pc flag stack ops)
+  (cond ((eq? (car inst) 'assign)
+         (make-assign inst machine labels ops pc))
+        ((eq? (car inst) 'test)
+         (make-test inst machine labels ops flag pc))
+        ((eq? (car inst) 'branch)
+         (make-branch inst machine labels flag pc))
+        ((eq? (car inst) 'goto)
+         (make-goto inst machine labels pc))
+        ((eq? (car inst) 'save)
+         (make-save inst machine stack pc))
+        ((eq? (car inst) 'restore)
+         (make-restore inst machine stack pc))
+        ((eq? (car inst) 'perform)
+         (make-perform inst machine labels ops pc))
+        (else (error "Unknown instruction type -- ASSEMBLE"
+                     inst))))
+
+ ;; assign
+ (define (make-assign inst machine labels operations pc)
+  (let ((target
+         (get-register machine (assign-reg-name inst)))
+        (value-exp (assign-value-exp inst)))
+    (let ((value-proc
+           (if (operation-exp? value-exp)
+               (make-operation-exp
+                value-exp machine labels operations)
+               (make-primitive-exp
+                (car value-exp) machine labels))))
+      (lambda ()                ; assignの実行手続き
+        (set-contents! target (value-proc))
+        (advance-pc pc)))))
+  
+(define (assign-reg-name assign-instruction)
+  (cadr assign-instruction))
+
+(define (assign-value-exp assign-instruction)
+  (cddr assign-instruction))
+
+(define (advance-pc pc)
+  (set-contents! pc (cdr (get-contents pc))))
+
+;;goto
+(define (make-test inst machine labels operations flag pc)
+  (let ((condition (test-condition inst)))
+    (if (operation-exp? condition)
+        (let ((condition-proc
+               (make-operation-exp
+                condition machine labels operations)))
+          (lambda ()
+            (set-contents! flag (condition-proc))
+            (advance-pc pc)))
+        (error "Bad TEST instruction -- ASSEMBLE" inst))))
+
+(define (test-condition test-instruction)
+  (cdr test-instruction))
+
+(define (make-branch inst machine labels flag pc)
+  (let ((dest (branch-dest inst)))
+    (if (label-exp? dest)
+        (let ((insts
+               (lookup-label labels (label-exp-label dest))))
+          (lambda ()
+            (if (get-contents flag)
+                (set-contents! pc insts)
+                (advance-pc pc))))
+        (error "Bad BRANCH instruction -- ASSEMBLE" inst))))
+
+(define (branch-dest branch-instruction)
+  (cadr branch-instruction))
+
+(define (make-goto inst machine labels pc)
+  (let ((dest (goto-dest inst)))
+    (cond ((label-exp? dest)
+           (let ((insts
+                  (lookup-label labels
+                                (label-exp-label dest))))
+             (lambda () (set-contents! pc insts))))
+          ((register-exp? dest)
+           (let ((reg
+                  (get-register machine
+                                (register-exp-reg dest))))
+             (lambda ()
+               (set-contents! pc (get-contents reg)))))
+          (else (error "Bad GOTO instruction -- ASSEMBLE"
+                       inst)))))
+
+
+(define (goto-dest goto-instruction)
+  (cadr goto-instruction))
+
+;;misc
+(define (make-save inst machine stack pc)
+  (let ((reg (get-register machine
+                           (stack-inst-reg-name inst))))
+    (lambda ()
+      (push reg (get-contents reg)) ;;各registerのstackへpush
+      (advance-pc pc))))
+
+(define (make-restore inst machine stack pc)
+  (let ((reg (get-register machine
+                           (stack-inst-reg-name inst))))
+    (lambda ()
+      (set-contents! reg (pop reg)) ;;各registerのstackからpop
+      (advance-pc pc))))
+
+
+(define (stack-inst-reg-name stack-instruction)
+  (cadr stack-instruction))
+
+ (define (make-perform inst machine labels operations pc)
+  (let ((action (perform-action inst)))
+    (if (operation-exp? action)
+        (let ((action-proc
+               (make-operation-exp
+                action machine labels operations)))
+          (lambda ()
+            (action-proc)
+            (advance-pc pc)))
+        (error "Bad PERFORM instruction -- ASSEMBLE" inst))))
+
+
+(define (perform-action inst) (cdr inst))
+
+;; 部分式
+
+(define (make-primitive-exp exp machine labels)
+  (cond ((constant-exp? exp)
+         (let ((c (constant-exp-value exp)))
+           (lambda () c)))
+        ((label-exp? exp)
+         (let ((insts
+                (lookup-label labels
+                              (label-exp-label exp))))
+           (lambda () insts)))
+        ((register-exp? exp)
+         (let ((r (get-register machine
+                                (register-exp-reg exp))))
+           (lambda () (get-contents r))))
+        (else
+         (error "Unknown expression type -- ASSEMBLE" exp))))
+
+;;reg, labelおよびconst式の構文
+
+(define (register-exp? exp) (tagged-list? exp 'reg))
+
+
+(define (register-exp-reg exp) (cadr exp))
+
+
+(define (constant-exp? exp) (tagged-list? exp 'const))
+
+
+(define (constant-exp-value exp) (cadr exp))
+
+
+(define (label-exp? exp) (tagged-list? exp 'label))
+
+
+(define (label-exp-label exp) (cadr exp))
+
+
+;; operation
+(define (make-operation-exp exp machine labels operations)
+  (let ((op (lookup-prim (operation-exp-op exp) operations))
+        (aprocs
+         (map (lambda (e)
+                (if (label-exp? e)
+                    (error "Cannot apply operation to label " e)
+                    (make-primitive-exp e machine labels)))
+              (operation-exp-operands exp))))
+    (lambda ()
+      (apply op (map (lambda (p) (p)) aprocs)))))
+
+
+(define (operation-exp? exp)
+  (and (pair? exp) (tagged-list? (car exp) 'op)))
+
+
+(define (operation-exp-op operation-exp)
+  (cadr (car operation-exp)))
+
+
+(define (operation-exp-operands operation-exp)
+  (cdr operation-exp))
+
+
+(define (lookup-prim symbol operations)
+  (let ((val (assoc symbol operations)))
+    (if val
+        (cadr val)
+        (error "Unknown operation -- ASSEMBLE" symbol))))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+
+(define test-machine
+  (make-machine
+    '(a b)
+    '()
+    '(start
+       (assign a (const 1))
+       (assign b (const 2))
+       (save a)
+       (save b)
+       (assign a (const 3))
+       (assign b (const 4))
+       (save a)
+       (assign a (const 5))
+       (restore a)
+       (restore a)
+       (restore b)
+       (goto (label done))
+      done)))
+
+(start test-machine)
+
+(get-register-contents test-machine 'a)
+(get-register-contents test-machine 'b)
---
+++ SICP5.2.md
@@ -0,0 +1,316 @@
+# 5.7
+
+## a 再帰
+計算機モデルの構成
+```
+(define expt-machine
+  (make-machine
+   '(b n counter product)
+   (list (list '= =) (list '- -) (list '* *))
+   '((assign continue (label expt-done))
+  expt-loop
+    (test (op =) (reg n) (const 0))
+    (branch (label base-case))
+    (save continue)
+    (save n)
+    (assign n (op -) (reg n) (const 1))
+    (assign continue (label after-expt))
+    (goto (label expt-loop))
+  after-expt
+    (restore n)
+    (assgin val (op *) (reg b) (reg val))
+    (goto (reg continue))
+  base-case
+    (assign val (const 1))
+    (goto (reg continue))
+  expt-done)))
+```
+
+テスト（実装がないので推測。こう動くはず）
+```
+(set-register-contents! expt-machine 'b 3)
+(set-register-contents! expt-machine 'n 4)
+(start expt-machine)
+(get-register-contents expt-machine 'product)
+=> 81
+```
+
+## b 反復
+```
+(define expt-machine
+  (make-machine
+   '(b n counter product)
+   (list (list '= =) (list '- -) (list '* *))
+   '((assign product (const 1))
+    (assign counter (reg n))
+  expt-loop
+    (test (op =) (reg counter) (const 0))
+    (branch (label expt-done))
+    (assign counter (op -) (reg counter) (const 1))
+    (assign product (op *) (reg b) (reg product))
+    (goto (label expt-loop))
+  expt-done)))
+```
+
+テスト（実装がないので推測。こう動くはず）
+```
+(set-register-contents! expt-machine 'b 3)
+(set-register-contents! expt-machine 'n 4)
+(start expt-machine)
+(get-register-contents expt-machine 'product)
+=> 81
+```
+
+# 5.8
+```
+(define (extract-labels text receive)
+  (if (null? text)
+      (receive '() '())
+      (extract-labels (cdr text)
+       (lambda (insts labels)
+         (let ((next-inst (car text)))
+           (if (symbol? next-inst)
+             (if (assoc next-inst labels)
+                 (error "label already exists :" next-inst) ;;ADD
+                 (receive insts
+                          (cons (make-label-entry next-inst
+                                                  insts)
+                                labels))
+                 (receive (cons (make-instruction next-inst)
+                                insts)
+                          labels)))))))
+```
+
+# 5.9
+```
+(define (make-operation-exp exp machine labels operations)
+  (let ((op (lookup-prim (operation-exp-op exp) operations))
+        (aprocs
+         (map (lambda (e)
+                (if (label-exp? e)
+                    (error "Cannot apply operation to label " e) ;;ADD
+                    (make-primitive-exp e machine labels)))
+              (operation-exp-operands exp))))
+    (lambda ()
+      (apply op (map (lambda (p) (p)) aprocs)))))
+```
+
+# 5.10
+新しい構文の具体例が思いつかなかったが、おそらく 11のcが概ね題意に沿っていると思う。
+
+(register に pop/pushの構文を追加した状態)
+
+# 5.11
+## a
+1命令を除去する = 1命令少なくしても同じ結果が得られる　という意味と理解。
+
+書き換え前
+```
+afterfib-n-2                         ; 戻った時Fib(n-2)の値はvalにある
+  (assign n (reg val))               ; nにはFib(n-2)がある
+  (restore val)                      ; valにはFib(n-1)がある
+  (restore continue)
+  (assign val                        ; Fib(n-1)+Fib(n-2)
+          (op +) (reg val) (reg n))
+  (goto (reg continue))              ; 呼出し側に戻る. 答えはvalにある
+```
+
+書き換え後
+```
+afterfib-n-2                         ; 戻った時Fib(n-2)の値はvalにある
+  (restore n)                        ; nにはFib(n-1)がある
+  (restore continue)
+  (assign val                        ; Fib(n-2)+Fib(n-1)
+          (op +) (reg val) (reg n))
+  (goto (reg continue))              ; 呼出し側に戻る. 答えはvalにある
+```
+
+val と n の値はテレコでも問題ないので、 nにスタックの値(Fib(n-1))を復元すると、1命令減る
+
+## b
+saveで、reg-nameを保存する。
+```
+(define (make-save inst machine stack pc)
+  (let ((reg-name  (stack-inst-reg-name inst)))
+     (let ((reg (get-register machine reg-name)))
+        (lambda ()
+           (push stack (cons reg-name (get-contents reg)))
+           (advance-pc pc)))))
+```
+
+restoreで、引数の reg-name と popされた値に格納された reg-name が一致することを確認する.
+```
+(define (make-restore inst machine stack pc)
+  (let ((reg-name  (stack-inst-reg-name inst)))
+     (let ((reg (get-register machine reg-name)))
+        (lambda ()
+           (let ((popped (pop stack)))
+              (if (eq? (car popped) reg-name)
+                  (set-contents! reg (cdr popped))
+                  (error "Wrong reg-name" reg-name)))
+           (advance-pc pc)))))
+```
+
+## c
+1つのスタックで連想配列的な実装をする案と、
+レジスタごとにスタックを持たせる案とが出てきた。
+
+題意に沿うのは前者だが、後者の方が実装がすっきりしそうなので後者を試してみる。
+
+全体は 5.11c.rkt
+
+make-registerの変更
+```
+(define (make-register name)
+  (let ((contents '*unassigned*)
+        (stack (make-stack))) ;; 各regster専用のstack
+    (define (dispatch message)
+      (cond ((eq? message 'get) contents)
+            ((eq? message 'set)
+             (lambda (value) (set! contents value)))
+            ((eq? message 'push) (stack 'push)) ;; 自分のregsterへpush
+            ((eq? message 'pop) (stack 'pop)) ;; 自分のregsterからpop
+            ((eq? message 'initialize) (stack 'initialize))
+            (else
+             (error "Unknown request -- REGISTER" message))))
+    dispatch))
+```
+
+make-new-machine 中の allocate-registerを修正
+```
+(define (make-new-machine)
+...
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register: " name)
+              (let ((reg (make-register name)))
+                (set! register-table (cons (list name reg) register-table))
+                (reg 'initialize))) ;initialzie the stack of allocated register
+        'register-allocated)
+```
+
+make-save/make-restoreの行き先をstackからregisterに
+```
+(define (make-save inst machine stack pc)
+  (let ((reg (get-register machine
+                           (stack-inst-reg-name inst))))
+    (lambda ()
+      (push reg (get-contents reg)) ;;各registerのstackへpush
+      (advance-pc pc))))
+
+(define (make-restore inst machine stack pc)
+  (let ((reg (get-register machine
+                           (stack-inst-reg-name inst))))
+    (lambda ()
+      (set-contents! reg (pop reg)) ;;各registerのstackからpop
+      (advance-pc pc))))
+```
+
+
+# 5.12
+
+全体は  5.12.rkt 参照.
+
+この方式の欠点は、一度実行しないと analyzerが完成しない点。
+
+
+make-new-machine に analyzerを追加
+```
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+       ..
+       (stack (make-stack))
+       (analyzer (make-analyzer)) ;; ADD
+       (the-instruction-sequence '()))
+    ..
+    (define (dispatch message)
+        (cond ((eq? message 'start)
+              ..
+              ((eq? message 'operations) the-ops)
+              ((eq? message 'analyzer) analyzer) ;; ADD
+              (else (error "Unknown request -- MACHINE" message))))
+..
+```
+
+analyzerの定義
+```
+;; 解析器
+(define (make-analyzer)
+  (let ((analyze-list '((assgin) (test) (branch) (goto) (save) (restore) (perform))))
+    (define (add-analyzer inst label)
+      (let (( analyzer (assoc label analyze-list)))
+        (if analyzer
+            (if (not (member inst analyzer))
+                (set-cdr! analyzer (cons inst (cdr analyzer)))))))
+    (define (print-analyzer) (print analyze-list))
+    (define (dispatch message)
+      (cond ((eq? message 'add) add-analyzer)
+            ((eq? message 'print) print-analyzer)
+            (else (error "Unkown message" message))))
+    dispatch))
+
+(define (print-analyzed-result machine)
+  (((machine 'analyzer) 'print)))
+
+(define (add-analyzer inst machine label)
+  (((machine 'analyzer) 'add) inst label))
+```
+
+実行時に analyzerに追加する
+```
+;; 実行手続き
+  (define (make-execution-procedure inst labels machine pc flag stack ops)
+  (cond ((eq? (car inst) 'assign)
+         (add-analyzer inst machine 'assgin)
+         (make-assign inst machine labels ops pc))
+        ((eq? (car inst) 'test)
+         (add-analyzer inst machine 'test)
+         (make-test inst machine labels ops flag pc))
+        ((eq? (car inst) 'branch)
+         (add-analyzer inst machine 'branch)
+         (make-branch inst machine labels flag pc))
+        ((eq? (car inst) 'goto)
+         (add-analyzer inst machine 'goto)
+         (make-goto inst machine labels pc))
+        ((eq? (car inst) 'save)
+         (add-analyzer inst machine 'save)
+         (make-save inst machine stack pc))
+        ((eq? (car inst) 'restore)
+         (add-analyzer inst machine 'restore)
+         (make-restore inst machine stack pc))
+        ((eq? (car inst) 'perform)
+         (add-analyzer inst machine 'perform)
+         (make-perform inst machine labels ops pc))
+        (else (error "Unknown instruction type -- ASSEMBLE"
+                     inst))))
+```
+
+# 5.13
+
+registerの操作は必ず lookup-registerを通るので、ココを修正する。
+
+全体ソースは 5.13.rkt
+
+make-machineの変更（register-names周りの処理を削除)
+```
+(define (make-machine ops controller-text)
+  (let ((machine (make-new-machine)))
+;    (for-each (lambda (register-name)
+;                ((machine 'allocate-register) register-name))
+;              register-names)
+    ((machine 'install-operations) ops)
+    ((machine 'install-instruction-sequence)
+     (assemble controller-text machine))
+    machine))
+```
+
+make-new-machineの中の lookup-register を修正
+```
+(define (lookup-register name)
+  (let ((val (assoc name register-table)))
+    (if val
+        (cadr val)
+        (begin ;無ければregisterを追加して再度lookup
+          (allocate-register name)
+          (lookup-register name)))))
+```
